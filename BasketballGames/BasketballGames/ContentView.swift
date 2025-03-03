2913f7b303a39d0b5bd4085a6ca65c9de5795e07
maximdolphin <maximka.dolphin@gmail.com>
2025-03-02 23:05:42 -0500

    add code

--- BasketballGames/BasketballGames/ContentView.swift
+++ BasketballGames/BasketballGames/ContentView.swift
@@ -7,15 +7,64 @@
 
 import SwiftUI
 
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var date: String
+    var opponent: String
+    var id: Int
+    var team: String
+    var isHomeGame: Bool
+    var score: Score
+}
+
+struct Score: Codable {
+    var unc: Int
+    var opponent: Int
+}
+
 struct ContentView: View {
+    @State private var games = [Result]()
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(games, id: \.id) { item in
+                HStack(alignment: .center) {
+                    VStack(alignment: .leading) {
+                        Text("\(item.team) vs. \(item.opponent)")
+                        Text(item.date)
+                            .font(.caption)
+                            .foregroundStyle(.gray)
+                    }
+                    Spacer()
+                    VStack(alignment: .trailing) {
+                        Text("\(item.score.unc) - \(item.score.opponent)")
+                        Text("\(item.isHomeGame ? "Home" : "Away")")
+                            .font(.footnote)
+                            .foregroundStyle(.gray)
+                    }
+                }
+            }
+            .navigationTitle("UNC Basketball Games")
+            .task {
+                await getData()
+            }
         }
-        .padding()
+    }
+    
+    func getData() async {
+        guard let url = URL(string: "https://api.samuelshi.com/uncbasketball") else {
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            if let decodedResponse = try? JSONDecoder().decode([Result].self, from: data) {
+                games = decodedResponse
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
